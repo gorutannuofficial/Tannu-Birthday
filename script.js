133b3856282eb27dca80bc5108b31a7a4a8ce01f
gorutannuofficial <gj04940@gmail.com>
2026-07-15 01:06:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,44 @@
-const correctPassword = "Tannu13Goru"; // 👈 Isko badal do
+const correctPassword = "BPS"; // Tannu13Goru
 
 setTimeout(() => {
     const userPassword = prompt("🔐 Enter Secret Password");
 
     if (userPassword === correctPassword) {
-        document.getElementById("loading").innerHTML = `
-            <h1>❤️ Welcome Tannu ❤️</h1>
-            <p>Birthday Surprise is Loading...</p>
-        `;
-    } else {
+
         document.body.innerHTML = `
-            <div style="display:flex;justify-content:center;align-items:center;height:100vh;background:black;color:white;font-family:Arial;">
-                <h2>❌ Wrong Password</h2>
-            </div>
+        <div style="text-align:center;padding:20px;background:#000;color:white;min-height:100vh;font-family:Arial;">
+
+            <h1 style="color:#ff4d6d;">🎉 Happy Birthday Tannu ❤️ 🎂</h1>
+
+            <h2>My Beautiful Wife ❤️</h2>
+
+            <p>
+            Aaj ka din sirf tumhare liye hai.<br><br>
+
+            Thank you meri life me aane ke liye.<br>
+            I Love You Forever ❤️
+            </p>
+
+            <br>
+
+            <img src="photo1.jpg" style="width:90%;max-width:350px;border-radius:20px;"><br><br>
+
+            <img src="photo2.jpg" style="width:90%;max-width:350px;border-radius:20px;"><br><br>
+
+            <img src="photo3.jpg" style="width:90%;max-width:350px;border-radius:20px;"><br><br>
+
+            <h2>❤️ Forever Together ❤️</h2>
+
+        </div>
         `;
+
+    } else {
+
+        document.body.innerHTML = `
+        <div style="display:flex;justify-content:center;align-items:center;height:100vh;background:black;color:white;font-family:Arial;">
+        <h2>❌ Wrong Password</h2>
+        </div>`;
+
     }
-}, 1000);
+
+},1000);

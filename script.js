2bc9991e187777adc7576f0a4591d9029aafdeab
gorutannuofficial <gj04940@gmail.com>
2026-07-15 01:38:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,43 +6,7 @@ setTimeout(() => {
 
     if (userPassword === correctPassword) {
 
-        document.body.innerHTML = `
-        <div style="background:black;color:white;min-height:100vh;padding:20px;text-align:center;font-family:Arial;">
-
-            <h1 style="color:#ff4d6d;font-size:45px;">
-            ❤️ Happy Birthday Tannu ❤️
-            </h1>
-
-            <p style="font-size:22px;">
-            Welcome My Love 💖
-            </p>
-
-            <br>
-
-            <img src="photo1.jpg" style="width:90%;max-width:350px;border-radius:20px;"><br><br>
-
-            <p style="font-size:18px;">
-            Aaj ka din meri life ka sabse special din hai...
-            Kyuki aaj meri duniya ka sabse khoobsurat insaan is duniya me aaya tha. ❤️
-            </p>
-
-            <br>
-
-            <img src="photo2.jpg" style="width:90%;max-width:350px;border-radius:20px;"><br><br>
-
-            <p style="font-size:18px;">
-            Thank You meri life me aane ke liye...
-            I Love You Forever ❤️
-            </p>
-
-            <br>
-
-            <img src="photo3.jpg" style="width:90%;max-width:350px;border-radius:20px;"><br><br>
-
-            <h2>💖 Forever Together 💖</h2>
-
-        </div>
-        `;
+        
 
     } else {
 

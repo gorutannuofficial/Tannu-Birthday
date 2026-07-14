9cd0aad25cdbe3c77f4c3dbc5c65d6d146285637
gorutannuofficial <gj04940@gmail.com>
2026-07-15 01:14:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,45 @@
-const correctPassword = "Tannu13Goru"; // 
+const correctPassword = "Tannu13Goru";
 
 setTimeout(() => {
+
     const userPassword = prompt("🔐 Enter Secret Password");
 
     if (userPassword === correctPassword) {
 
         document.body.innerHTML = `
-        <div style="text-align:center;padding:20px;background:#000;color:white;min-height:100vh;font-family:Arial;">
+        <div style="background:black;color:white;min-height:100vh;padding:20px;text-align:center;font-family:Arial;">
 
-            <h1 style="color:#ff4d6d;">🎉 Happy Birthday Tannu ❤️ 🎂</h1>
+            <h1 style="color:#ff4d6d;font-size:45px;">
+            ❤️ Happy Birthday Tannu ❤️
+            </h1>
 
-            <h2>My Beautiful Wife ❤️</h2>
-
-            <p>
-            Aaj ka din sirf tumhare liye hai.<br><br>
-
-            Thank you meri life me aane ke liye.<br>
-            I Love You Forever ❤️
+            <p style="font-size:22px;">
+            Welcome My Love 💖
             </p>
 
             <br>
 
             <img src="photo1.jpg" style="width:90%;max-width:350px;border-radius:20px;"><br><br>
 
+            <p style="font-size:18px;">
+            Aaj ka din meri life ka sabse special din hai...
+            Kyuki aaj meri duniya ka sabse khoobsurat insaan is duniya me aaya tha. ❤️
+            </p>
+
+            <br>
+
             <img src="photo2.jpg" style="width:90%;max-width:350px;border-radius:20px;"><br><br>
+
+            <p style="font-size:18px;">
+            Thank You meri life me aane ke liye...
+            I Love You Forever ❤️
+            </p>
+
+            <br>
 
             <img src="photo3.jpg" style="width:90%;max-width:350px;border-radius:20px;"><br><br>
 
-            <h2>❤️ Forever Together ❤️</h2>
+            <h2>💖 Forever Together 💖</h2>
 
         </div>
         `;
@@ -35,9 +47,10 @@ setTimeout(() => {
     } else {
 
         document.body.innerHTML = `
-        <div style="display:flex;justify-content:center;align-items:center;height:100vh;background:black;color:white;font-family:Arial;">
-        <h2>❌ Wrong Password</h2>
-        </div>`;
+        <div style="display:flex;justify-content:center;align-items:center;height:100vh;background:black;color:white;font-size:30px;">
+        ❌ Wrong Password
+        </div>
+        `;
 
     }
 
